977ab14939474484be85c067a3fe9c342960495a
Agus <AgustinSilvaJP@gmail.com>
2024-06-22 16:45:55 -0300

    creacion clase Dog, con sus respetivos constructores, metodos, setter y getters

--- Practice-2/src/Dog.java
+++ Practice-2/src/Dog.java
@@ -1,2 +1,25 @@
-public class Dog {
+public class Dog{
+    private String name;
+    private String breed; //breed = raza;
+
+    public Dog(String name, String breed){
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getBreed(){
+        return breed;
+    }
+    public String getInfo(){
+        return "Name: " + getName() + ", breed: " + getBreed();
+    }
+    public void putName(String name){
+        this.name = name;
+    }
+    public void putBreed(String breed){
+        this.breed = breed;
+    }
 }
