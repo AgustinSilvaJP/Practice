27821719fce87caec696a53f870064af7e9c7245
Agus <AgustinSilvaJP@gmail.com>
2024-06-22 16:51:26 -0300

    asssd

--- Practice-2/src/Main.java
+++ Practice-2/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner uwu = new Scanner(System.in);
 
+        Dog d1 = new Dog();
+        Dog d2 = new Dog();
+
+        d1.setName(uwu.nextLine());
+        d1.setBreed(uwu.nextLine());
+        System.out.println(d1.getInfo());
+
+        System.out.println();
+
+        d2.setName(uwu.nextLine());
+        d2.setBreed(uwu.nextLine());
+        System.out.println(d2.getInfo());
     }
 }
