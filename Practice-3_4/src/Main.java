8eb74e140b803e84c7bef0b9afbefd9216a3b672
Agus <AgustinSilvaJP@gmail.com>
2024-06-23 16:37:55 -0300

    add circle things

--- Practice-3_4/src/Main.java
+++ Practice-3_4/src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Circle...");
+        System.out.println("Type the radius of the circle:");
+        Circle circle = new Circle(scan.nextDouble());
 
+        System.out.printf("The area of the circle of radius %.2f is: %.2f\n", circle.getRadius(), circle.getArea());
+        System.out.printf("The circunference of the circle is: %.2f\n", circle.getCircunference());
     }
 }
