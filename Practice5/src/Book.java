2e0005c8c6bfc4c3d7aa564d9f5b63fe29943113
Agus <AgustinSilvaJP@gmail.com>
2024-06-23 16:53:03 -0300

    Class Book created

---
+++ Practice5/src/Book.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Book {
+    private String tittle;
+    private String author;
+    private int isbn;
+    private static ArrayList<Book> books = new ArrayList<Book>();
+
+    
+}
