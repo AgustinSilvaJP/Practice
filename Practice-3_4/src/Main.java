ce9191c092fa954004c0917516076dd984691254
Agus <AgustinSilvaJP@gmail.com>
2024-06-23 16:42:47 -0300

    add some code to test classes

--- Practice-3_4/src/Main.java
+++ Practice-3_4/src/Main.java
@@ -10,5 +10,14 @@ public class Main {
 
         System.out.printf("The area of the circle of radius %.2f is: %.2f\n", circle.getRadius(), circle.getArea());
         System.out.printf("The circunference of the circle is: %.2f\n", circle.getCircunference());
+
+        System.out.println("---------------------------------------------------------------");
+
+        System.out.println("Rectangle...");
+        System.out.println("Type the width and height of the rectangle:");
+        Rectangle rect = new Rectangle(scan.nextDouble(), scan.nextDouble());
+
+        System.out.printf("area and perim. of the Reactangle of width %.2f and heigth %.2f\n", rect.getWidth(), rect.getHeight());
+        System.out.printf("Area: %.2f, Perimeter: %.2f\n", rect.getArea(), rect.getPerimeter());
     }
 }
